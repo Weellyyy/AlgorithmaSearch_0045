4d8254abea87306d19e3e374180c00c44d6ffd2c
Weellyyy <wildanprtm2304@gmail.com>
2024-04-04 16:56:02 +0700

    menambahkan sisa dari prosedur input

--- Search/Search.cpp
+++ Search/Search.cpp
@@ -16,5 +16,12 @@ void input() {
 			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
 	}
 
-	
+	// Accept array elements
+	cout << "\n----------\n";
+	cout << " Enter array elements \n";
+	cout << "----------\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << "> ";
+	}
 }
